8d39281d64c030a770aa4af9bd86b0adfd32b960
NicholasMiguel <NicholasMiguel>
2024-10-12 13:09:17 +0700

    SplashScreen1

---
+++ app/src/main/java/com/example/uts_map/SplashScreen1.kt
@@ -0,0 +1,24 @@
+package com.example.uts_map
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen1 : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DURATION = 3000L // 3 seconds
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen1)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DURATION)
+    }
+}
