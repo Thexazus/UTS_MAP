ba91237c0b07bf8e293d391e6e1ba877200042f0
Thexazus <98933222+Thexazus@users.noreply.github.com>
2024-10-13 15:58:19 +0700

    update main activity

--- app/src/main/java/com/example/uts_map/MainActivity.kt
+++ app/src/main/java/com/example/uts_map/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+                findViewById<BottomNavigationView>(R.id.bottomNavigation).setupWithNavController(navController)
+            }
+        }
         }
     }
 }
