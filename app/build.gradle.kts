aea2d0a38ffdd63b0f22a44e25afb27e0b036478
Thexazus <98933222+Thexazus@users.noreply.github.com>
2024-10-24 02:20:15 +0700

    update report

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.mikhaellopez:circularprogressbar:3.1.0")
 }
