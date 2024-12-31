e431e1f9157041b1c2e4f8091bb3fd3c49129a22
Relchzen <104548363+Relchzen@users.noreply.github.com>
2024-12-31 14:31:01 +0700

    add delete button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.storage.ktx)
+    implementation(libs.androidx.compose.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
